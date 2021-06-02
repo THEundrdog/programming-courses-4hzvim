e7190f56738132f120f730e9eee0c2cbf503449b
THEundrdog <chakibtheunderdog@gmail.com>
2021-06-01 19:38:48 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -5,8 +5,13 @@
 let num1: number;
 let num2: number;
 let num3: number;
-num1 = 40;
-num2 = 50;
+let num4: number;
+let num5: number;
+num1 = 10;
+num2 = 30;
+num3 = 50;
+num4 = 10;
+num5 = num1 + num2 + num3 + num4 ;
 
-num3 = num1 + num2 ;
-console.log(num3);
+console.log(num5);
+
