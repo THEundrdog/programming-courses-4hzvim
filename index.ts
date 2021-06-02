4e9dd86b4dadf33c3906ad278ba5fb022778c119
THEundrdog <chakibtheunderdog@gmail.com>
2021-06-01 17:18:00 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,13 +1,7 @@
 //######################################################################
 //###########################           SSTech       ###################
 //###########################  
- let num1: number;
- let num2: number;
- num1 = 123;
- num2 = 457;
- console.log(num1);
- console.log(num2);
- console.log (num1 + num2);
+
  let num3: number;
  let num4: number;
  num3 = 100;
