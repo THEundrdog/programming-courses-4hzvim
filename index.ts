7493568a2206653d2a0654c6b4e7ed36d4b42500
THEundrdog <chakibtheunderdog@gmail.com>
2021-06-09 02:30:57 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@
 //###########################  
 
 let mynumber1, mynumber2, mynumber3: number;
-mynumber1 = 50;
-mynumber2 = 110;
+mynumber1 = parseInt( prompt ("المرجو ادخال العدد الأول"));
+mynumber2 = parseInt ( prompt ("المرجو ادخال العدد الثاني"));
 mynumber3 = mynumber1 + mynumber2;
 console.log(mynumber3);
