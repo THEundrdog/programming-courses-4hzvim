adeb0c7cc696f14ee46c7ec058337089d9405285
THEundrdog <chakibtheunderdog@gmail.com>
2021-06-10 01:04:58 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
  //###################################################################### 
-//###########################           SSTech       ################### 
+//###########################           SSTech       ###################  
 //###########################  
   
- let mynumber1, resulte : number;
- mynumber1 = parseInt(prompt("مرجو كتابة العدد الاول"));
- resulte = mynumber1 % 2;
- if(resulte == 0 ) { 
-   console.log("العدد زوجي") ;
-} else { 
-   console.log("العدد فردي")
-}  
+ let age : number;
+ age = parseInt(prompt("مرجو كتابة العمر"));
+if(age == 1  || age == 2) {
+  console.log("رضيع");
+   }else if(age  >= 3 && age <= 14) {  
+    console.log("طفل");
+  } else if(age >= 15 && age <= 21) {
+    console.log("شاب");
+  } eles if(age >= 22 ) {
+    console.log("رجل");
+   } 
+ 
+    
