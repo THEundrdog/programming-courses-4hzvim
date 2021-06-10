7fa20706d2880aa2c2869072945b6574d0bf1bc7
THEundrdog <chakibtheunderdog@gmail.com>
2021-06-09 14:18:13 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -5,10 +5,10 @@
  let mynumber1, mynumber2, result : number;
  mynumber1 = parseInt(prompt("مرجو كتابة العدد الأول"));
  mynumber2 = parseInt(prompt("مرجو كتابة العدد الثاني"));
- if(mynumber1 > mynumber2) {
+ if(mynumber1 > mynumber2) { 
   result = mynumber1 - mynumber2;
   }else {   
-    result = mynumber2 - mynumber2;
+    result = mynumber2 - mynumbe1;
     }
     console.log(result);
 
