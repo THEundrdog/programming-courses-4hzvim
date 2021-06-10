b29e2cbca719f9e6ee5cf547c786cb85b1777a35
THEundrdog <chakibtheunderdog@gmail.com>
2021-06-09 23:50:41 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -3,9 +3,10 @@
 //###########################  
   
  let mynumber1, resulte : number;
+ mynumber1 = parseInt(prompt("مرجو كتابة العدد الاول"));
  resulte = mynumber1 % 2;
  if(resulte == 0 ) { 
-  console.log("العدد زوجي") ;
-  } else {
-console.log("العدد فردي");
+   console.log("العدد زوجي") ;
+} else {
+   console.log("العدد فردي")
 }  
