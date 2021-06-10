88668880afa03a055b38c3d251ad65bb786158d6
THEundrdog <chakibtheunderdog@gmail.com>
2021-06-09 14:16:15 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@
  mynumber2 = parseInt(prompt("مرجو كتابة العدد الثاني"));
  if(mynumber1 > mynumber2) {
   result = mynumber1 - mynumber2;
-  }else {  
+  }else {   
     result = mynumber2 - mynumber2;
     }
     console.log(result);
