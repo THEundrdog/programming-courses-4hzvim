1e7e73f97ccd673211c84a9bbf4a3c3b22ea089a
THEundrdog <chakibtheunderdog@gmail.com>
2021-06-09 18:09:09 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -6,8 +6,8 @@
  mynumber1 = parseInt(prompt("مرجو كتابة العدد الاول"));
  mynumber2 = parseInt(prompt("ضرب العدد الثاني"));
  if(mynumber1 > mynumber2) {
-   resulte = mynumber1 - mynumber2;
+   resulte = mynumber1 / mynumber2;
    }else { 
-resulte = mynumber2 - mynumber1;
+resulte = mynumber2 / mynumber1;
 }
 console.log(resulte); 
