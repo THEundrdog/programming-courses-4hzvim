08ec92423d18974de3e0cdcee2d598ab13845c7c
THEundrdog <chakibtheunderdog@gmail.com>
2021-06-09 22:45:05 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -4,10 +4,10 @@
   
  let mynumber1, mynumber2, resulte : number;
  mynumber1 = parseInt(prompt("مرجو كتابة العدد الاول"));
- mynumber2 = parseInt(prompt("ضرب العدد الثاني"));
- if(mynumber1 > mynumber2) {
-   resulte = mynumber1 / mynumber2;
-   }else { 
-resulte = mynumber2 / mynumber1;
-}
-console.log(resulte); 
+ mynumber2 = parseInt(prompt("مرجو كتابة العدد الثاني"));
+ resulte = mynumber1 * mynumber2;
+ if(resulte > 0 ) { 
+console.log("ضرب العددين موجب"); 
+} else {
+  console.log("ضرب العددين سالب");
+  }
