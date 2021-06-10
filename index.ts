3de600b4f0ec4ce966962d2d88f23ac9b6dcc9c9
THEundrdog <chakibtheunderdog@gmail.com>
2021-06-09 13:25:21 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -5,9 +5,10 @@ let mynumber1, mynumber2, result : number;
 mynumber1 = parseInt(prompt("مرجو كتابة العدد الاول "));
 mynumber2 = parseInt(prompt("مرجو كتابة العدد الثاني"));
 result = mynumber1 * mynumber2;
-if(result == 0 ); {
+if(result == 0 ) {
   console.log("ضرب العددين منعدم");
-  }else if(result > 0 ) {  
+  }else 
+  if(result > 0 ) {  
   console.log("ضرب العددين موجب"); 
   }else {
     console.log("ضرب العددين سالب");
