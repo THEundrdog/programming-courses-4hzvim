8e30f0c284169cfac55d7c0729d5e4829c7be6c5
THEundrdog <chakibtheunderdog@gmail.com>
2021-06-12 06:35:40 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -2,12 +2,7 @@
 //###########################           SSTech       ###################  
 //###########################  
   
- let age, result : number;
- age = parseInt(prompt("مرجو كتابة عمر"));
- if(age == 1 || age == 2 ){
-   console.log("رضيع") ;
- } else if(age >= 3 && age  <= 14){
- console.log("طفل");
-} else if(age >= 15 && age <=21) { 
-  console.log("شاب");
-  }    
+ let index, number;
+for (index = 1; index <= 100; index ++ ) {
+console.log("الله اكبر" , index ) ; 
+} 
